cd38fee08a45aea6e9814a043367d7ba0a29f693
AlexVorozheykin <alex.kzn777@gmail.com>
2016-11-07 00:26:58 +0300

    add Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,12 @@
 public class Calculate {
-	public static void main(String[] arg){
+	public static void main(String[] args){
 		System.out.println("Calculate...");
+		int one = Integer.valueOf(args[0]);
+		int two = Integer.valueOf(args[1]);
+		int one1 = 10;
+		int two1 = 13;
+		int summ = one+two;
+		int summ1 = one1+two1;
+		System.out.println("Sum "+summ+"Summ1 "+summ1);
 	}
 }
